2a785dfe3b425338862ffb88e7ce9a46274ba2ae
DELL <ansariawaisraees@gamil.com>
2024-03-27 16:51:28 +0530

    Solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,27 @@
-import React, {useEffect, useState} from "react";
+
+import React , {useEffect, useState} from "react"
+
+
 
 const App = () => {
-    const [change, setChange] = useState("");
-
+  const[change , setChange] = useState("")
     useEffect(() => {
-        document.getElementById("markDown").innerText = change;
+        document.getElementById("markDown").innerText = change
     }, [change])
 
     return (
         <div className="app">
-            <div className="markDownContainer">
-                <textarea 
-                    value={change}
-                    onChange={(e) => setChange(e.target.value)}
-                />
-            </div>
-            <div className="preview">
-                <h1>
-                    <b id="markDown"></b>
-                </h1>
-            </div>       
+          <div className="markDownContainer">
+            <textarea 
+            value={change} 
+            onChange={(e) => setChange(e.target.value)}
+            />
+          </div>
+          <div className="preview">
+            <h1 >
+                <b id="markDown"></b>
+            </h1>
+          </div>
         </div>
     )
 }
